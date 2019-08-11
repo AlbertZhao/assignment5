f0a85029619bd13330577d14daaa894b9b226420
albert.zhao <albert.chiu.cn@gmail.com>
2019-08-11 23:58:32 +0800

    Initial commit

---
+++ src/main/java/com/zhaoshijie/controller/SecurityController.java
@@ -0,0 +1,25 @@
+package com.zhaoshijie.controller;
+
+import com.zhaoshijie.domain.LoginVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("security")
+public class SecurityController {
+
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<LoginVO> loginCheck(HttpServletRequest request, @RequestBody LoginVO loginVO) {
+
+        return new ResponseEntity<>(loginVO,HttpStatus.OK);
+    }
+
+
+}
---
+++ src/main/java/com/zhaoshijie/domain/LoginVO.java
@@ -0,0 +1,23 @@
+package com.zhaoshijie.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonInclude
+public class LoginVO {
+
+    @JsonProperty("username")
+    private String userName;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("email")
+    private String email;
+
+}
